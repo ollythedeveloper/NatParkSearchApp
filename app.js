0449d85bfb0a5f9869051c196c439dcc6f0e8008
Olivia Lewis <ollybaby12@gmail.com>
2020-12-31 23:26:30 -0500

    added js functions

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+const rootURL = 'https://developer.nps.gov/api/v1/parks?'
+const myAPI = 'n8HqTggp5FckAzPEjE1CRl6Tc5longlwqScWIj3Y'
+
+function displayResults(responseJson){
+    console.log(responseJson)
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++){
+    $('#results-list').append(
+      `<li><h3><a href=${responseJson.data[i].url}>${responseJson.data[i].fullName}</a></h3>
+      <p>${responseJson.data[i].description}</p>
+      <h4>States: ${responseJson.data[i].states}</h4>
+      </li>`
+    )};
+  $('#results').removeClass('hidden');
+};
+
+
+function getNationalParks(state1, state2, state3){
+  const url = rootURL + 'stateCode=' + state1+ '&stateCode=' + state2 + '&stateCode=' + state3 + '&api_key='+myAPI;
+  fetch(url)
+  .then(response => {
+    if(response.ok){
+      return response.json();
+    }
+    //throw new Error(response.statusText)
+  })
+  .then(responseJson => displayResults(responseJson));
+  //.catch(error => alert('Something went wrong:');
+}
+
+
+function watchForm(){
+  $('#js-form').submit(event => {
+    event.preventDefault();
+    const state1 = $('#js-state1').val();
+    const state2 = $('#js-state2').val();
+    const state3 = $('#js-state3').val();
+    getNationalParks(state1, state2, state3);
+  });
+}
+
+
+$(function(){
+  console.log('App loaded!');
+  watchForm();
+});
